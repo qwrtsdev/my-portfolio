541633a8ef8abbaf0fa922c9328ae36e7c8d4d45
Qwerytx_ <potatoloadzch@gmail.com>
2024-11-09 22:27:07 +0700

    chores: fix metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,17 @@
 import "./styles/globals.css";
 
-// TODO: site's metadata for infomations
+// site's metadata for displaying infomations (SEO)
 export const metadata = {
     title: 'QWRTS.DEV',
-    description: 'A portfolio site for showcase all of the works from a junior front-end developer. Where everything happens.',
+    description: 'Welcome to my portfolio – where my creativity come alive. Discover the projects of me who passionate about building beautiful, interactive experiences.',
     keywords: ['qwrt', 'qwrts', 'qwrtsdev', 'portfolio', 'qwertyx'],
     creator: 'Nuttapong Nakawirot',
     publisher: 'Nuttapong Nakawirot',
 
     openGraph: {
         title: 'QWRTS.DEV',
-        description: 'A portfolio site for showcase all of the works from a junior front-end developer. Where everything happens.',
-        url: 'https://qwrtsdev.vercel.com',
-        siteName: 'QWRTS.DEV',
+        description: 'Welcome to my portfolio – where my creativity come alive. Discover the projects of me who passionate about building beautiful, interactive experiences.',
+        url: 'https://qwrtsdev.vercel.app',
         images: {
             url: 'https://media.discordapp.net/attachments/1304513230244413460/1304520143853850665/qwrtscover.png?ex=672fb08f&is=672e5f0f&hm=fc8a6ecc61320ca67b220b6f1bd8756c1df6d9eeb7581ffbe971eb5a8c7482b2&=&format=webp&quality=lossless&width=912&height=479',
             width: 1200,
